dcb4a8d98192b883239bfaf8b79bf90099bddb01
yukihane <yukihane.feather@gmail.com>
2021-01-30 16:34:21 +0900

    defaultプロファイルでclientとして動作するように

--- client/src/main/java/com/example/oidcexample/GavaidpConfig.java
+++ client/src/main/java/com/example/oidcexample/GavaidpConfig.java
@@ -3,14 +3,12 @@ package com.example.oidcexample;
 import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
 
-@Profile("gavaidp")
 @Configuration
 public class GavaidpConfig {
 
