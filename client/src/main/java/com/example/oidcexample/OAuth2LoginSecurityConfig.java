87594e4bc9ce60f32a25aebda1fa918cf17089a7
yukihane <yukihane.feather@gmail.com>
2021-02-01 23:19:43 +0900

    tokenエンドポイントの通信をログ出力する

--- client/src/main/java/com/example/oidcexample/OAuth2LoginSecurityConfig.java
+++ client/src/main/java/com/example/oidcexample/OAuth2LoginSecurityConfig.java
@@ -1,17 +1,26 @@
 package com.example.oidcexample;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
+import org.springframework.web.client.RestTemplate;
 
 // https://docs.spring.io/spring-security/site/docs/5.4.2/reference/html5/#oauth2login-provide-websecurityconfigureradapter
 @EnableWebSecurity
@@ -22,7 +31,27 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests(authorize -> authorize
                 .antMatchers("/secure/**").authenticated())
-            .oauth2Login(withDefaults());
+            .oauth2Login(oauth2 -> {
+                // https://docs.spring.io/spring-security/site/docs/5.4.2/reference/html5/#oauth2login-advanced
+                oauth2.tokenEndpoint(endpoint -> {
+                    endpoint.accessTokenResponseClient(accessTokenResponseClient());
+                });
+            });
+    }
+
+    private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        final Logger logger = LoggerFactory
+            .getLogger(DefaultAuthorizationCodeTokenResponseClient.class.getName() + ".RestTemplate");
+
+        // DefaultAuthorizationCodeTokenResponseClient デフォルトコンストラクタより
+        final RestTemplate restTemplate = new RestTemplate(Arrays.asList(
+            new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        restTemplate.setInterceptors(List.of(new RestTemplateLoggingInterceptor(logger)));
+
+        final DefaultAuthorizationCodeTokenResponseClient ret = new DefaultAuthorizationCodeTokenResponseClient();
+        ret.setRestOperations(restTemplate);
+        return ret;
     }
 
     // https://docs.spring.io/spring-security/site/docs/5.4.2/reference/html5/#oauth2login-advanced-idtoken-verify
