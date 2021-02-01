0fbcd40699ed5f9847a8ad2556f9f2eeb9140d93
yukihane <yukihane.feather@gmail.com>
2021-02-02 00:16:25 +0900

    getBody()で例外がなげられることを想定した実装

--- client/src/main/java/com/example/oidcexample/RestTemplateLoggingInterceptor.java
+++ client/src/main/java/com/example/oidcexample/RestTemplateLoggingInterceptor.java
@@ -46,22 +46,25 @@ public class RestTemplateLoggingInterceptor implements ClientHttpRequestIntercep
             throw e;
         }
 
-        try (
-            final ClientHttpResponse respWrapper = new BufferingClientHttpResponseWrapper(response);
-            final BufferedReader reader = new BufferedReader(
-                new InputStreamReader(respWrapper.getBody(), "UTF-8"))) {
+        final ClientHttpResponse respWrapper = new BufferingClientHttpResponseWrapper(response);
+        final int statusCode = respWrapper.getRawStatusCode();
+        final HttpHeaders headers = respWrapper.getHeaders();
+        try {
+            try (
+                final BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(respWrapper.getBody(), "UTF-8"))) {
 
-            if (logger.isDebugEnabled()) {
-                final int statusCode = respWrapper.getRawStatusCode();
-                final HttpHeaders headers = respWrapper.getHeaders();
-                final String bodyStr = reader.lines().collect(Collectors.joining());
+                if (logger.isDebugEnabled()) {
+                    final String bodyStr = reader.lines().collect(Collectors.joining());
 
-                logger.debug("End requesting. statusCode: {}, headers: {}, body: {}", statusCode, headers, bodyStr);
+                    logger.debug("End requesting. statusCode: {}, headers: {}, body: {}", statusCode, headers, bodyStr);
+                }
+
             }
-
-            return respWrapper;
+        } catch (final IOException e) {
+            // 正常フローでも respWrapper.getBody() が IOException を投げる場合がある
+            logger.debug("End requesting. statusCode: {}, headers: {}}", statusCode, headers, e);
         }
-
+        return respWrapper;
     }
-
 }
